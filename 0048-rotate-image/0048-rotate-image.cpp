babba80b7a27f7270eacd41e19e3d77d3fc617d2
Om Salunkhe <omsalunkhe2007@gmail.com>
2026-05-07 19:25:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -12,12 +12,7 @@ public:
        
       }
        matrix=v;
-      for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<matrix[i][j]<<",";
-        }
-        cout<<endl;
-      }
+      
     
        
 
