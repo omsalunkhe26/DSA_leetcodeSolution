e1ee9a428335e3cba07b6e00b9d139618ad14bf0
Om Salunkhe <omsalunkhe2007@gmail.com>
2026-07-16 19:47:06 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.21%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,13 +3,11 @@ public:
     int singleNumber(vector<int>& nums) {
       sort(nums.begin(),nums.end());
       int n = nums.size();
-      if(n==1) return nums[0];
-      int i=1;
-        while(i<n){
-            if(nums[i-1]!=nums[i]) return nums[i-1];
-            i+=2;
-        }
-        return nums[n-1];
+     int xorr=0;
+     for(int i=0;i<n;i++){
+        xorr=xorr^nums[i];
+     }
+     return xorr;
         
     }
 };
