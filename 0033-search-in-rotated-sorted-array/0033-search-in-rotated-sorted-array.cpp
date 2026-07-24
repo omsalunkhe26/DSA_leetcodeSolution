09a9decd4ae0e47b3316858eb108c3d266471550
Om Salunkhe <omsalunkhe2007@gmail.com>
2026-07-24 23:05:23 +0530

    Time: 0 ms (100%), Space: 15.2 MB (68.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-      
-    int search(vector<int>& nums, int target,int si,int ei) {
-        if(si>ei) return -1;
-            int mid=si+(ei-si)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            
-        if(nums[si]<=nums[mid]){
-            if(nums[si]<=target && target<=nums[mid]){
-               return search(nums,target,si,mid-1);
-            }
-            else{
-               return  search(nums,target,mid+1,ei);
-            }
-        }
-        else{
-            if(target>=nums[mid] && target<=nums[ei]){
-            return search(nums,target,mid+1,ei);
-            }
-            else{
-                return  search(nums,target,si,mid-1);
-            }
-        }
-       
-    }
     int search(vector<int>&nums,int target){
-        return search(nums,target,0,nums.size()-1);
+        int si=0,ei=nums.size()-1;
+      
+      while(si<=ei){
+        int mid=si+(ei-si)/2;
+
+         if(target==nums[mid]){
+            return mid;
+         }
+         if(nums[si]==nums[mid] && nums[mid]==nums[ei]){
+            si++;
+            ei--;
+         }
+         else if(nums[mid]>=nums[si]){
+            if(target>=nums[si] && target<nums[mid]){
+                ei=mid-1;
+            }
+            else{
+                si=mid+1;
+            }
+
+         }
+         else{
+             if(target>nums[mid] && target<=nums[ei]){
+                si=mid+1;
+            }
+            else{
+                ei=mid-1;
+            }
+         }
+      }
+      return -1;
+
       }
     
 };
